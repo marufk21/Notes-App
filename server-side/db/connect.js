415f96cf6c59644ab17fb0f6ca4c01b5f02fc4ec
Maruf Khan <marufkhan2002@gmail.com>
2022-12-10 01:49:11 +0530

    setup-backend-express-mongo

---
+++ server-side/db/connect.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+main().catch((err) => console.log(err))
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/notes-app');
+}
+console.log('Connection is Successful');
