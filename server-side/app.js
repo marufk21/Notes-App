917fff409a840eeae505fd7c54ad5d01b058dded
Maruf Khan <marufkhan2002@gmail.com>
2022-12-10 04:27:43 +0530

    schemas-routes-storedata

--- server-side/app.js
+++ server-side/app.js
@@ -3,9 +3,11 @@ const app = express()
 const port = 3000
 require('./db/connect')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+
+//Routes
+app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
--- server-side/db/connect.js
+++ server-side/db/connect.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
+const mongoURI = 'mongodb://127.0.0.1:27017/notes-app'
+
 main().catch((err) => console.log(err))
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/notes-app');
+  await mongoose.connect(mongoURI);
 }
 console.log('Connection is Successful');
