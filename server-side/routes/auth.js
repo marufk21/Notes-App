3b71b40c5b304d5ded051eb64dbb09ec72b35fda
Maruf Khan <marufkhan2002@gmail.com>
2022-12-11 01:17:59 +0530

    jwt-loginroute

--- server-side/routes/auth.js
+++ server-side/routes/auth.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
-
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const { body, validationResult } = require('express-validator')
+
+const JWT_SECRET = 'Marufisagoodb$oy'
 
 // Create a User using: POST "/api/auth/createuser". No login required
 router.post(
@@ -28,16 +31,74 @@ router.post(
           .status(400)
           .json({ error: 'Sorry a user with this email already exists' })
       }
+
+      //Hashing a password using bcryptjs
+      const salt = await bcrypt.genSalt(10)
+      const securepass = await bcrypt.hash(req.body.password, salt)
+
       // Create a new user
       user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: securepass,
         email: req.body.email,
       })
-      res.json(user)
+
+      //jwt
+      const data = {
+        user: {
+          id: user.id,
+        },
+      }
+      const authtoken = jwt.sign(data, JWT_SECRET)
+      // res.json(user)
+      res.json({ authtoken })
     } catch (error) {
       console.error(error.message)
-      res.status(500).send('Some Error occured')
+      res.status(500).send('Internal Server Error')
+    }
+  },
+)
+
+// Authenticate a User using: POST "/api/auth/login". No login required
+router.post(
+  '/login',
+  [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+  ],
+  async (req, res) => {
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body
+    try {
+      let user = await User.findOne({ email })
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: 'Please try to login with correct credentials' })
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password)
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: 'Please try to login with correct credentials' })
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      }
+      const authtoken = jwt.sign(data, JWT_SECRET)
+      res.json({ authtoken })
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send('Internal Server Error')
     }
   },
 )
