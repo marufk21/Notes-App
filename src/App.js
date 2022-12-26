7d0e8e5463f45fa5327a8bc810d804d2f09ba14a
Maruf Khan <marufkhan2002@gmail.com>
2022-12-27 00:48:52 +0530

    react-router-dom-v6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import About from './Components/About'
 
 const App = () => {
   return (
-    <div className='text-5xl font-bold'>Hello World</div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </>
   )
 }
 
