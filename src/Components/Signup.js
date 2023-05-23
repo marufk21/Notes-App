db63bb953ef2a88eaa00db0c859fbedcf1e7e0fc
Maruf Khan <marufkhan2002@gmail.com>
2023-05-23 15:31:19 +0530

    deploy

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -13,8 +13,8 @@ const Signup = (props) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { name, email, password, cpassword } = credentials;
-    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+    // const { name, email, password, cpassword } = credentials;
+    const response = await fetch("http://http://localhost:3000/api/auth/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
